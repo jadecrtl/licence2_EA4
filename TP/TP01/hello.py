8878507ff3ee0029c2812ce9e39cac6a98d1ac12
jadecrtl <jadecortial@gmail.com>
2022-01-31 21:49:31 +0100

    cours

---
+++ TP/TP01/hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def affiche() :
+    print("Hello World!")
+    print("Bonjour le monde!")
+
+def dev() :
+    print("Developpeur : Jade")
---
+++ essai/test1.py
@@ -0,0 +1,25 @@
+print(9 ** (1/2))
+
+print(20//6)
+
+print("""this
+is a
+multiline
+text""")
+
+print("spam" + 'eggs')
+
+print("2" + "2")
+
+print("spam" * 3)
+
+x = input("Entrer quelque chose : ")
+print(x)
+
+age = int(input("Entrer un int : "))
+print(age)
+
+l = 42
+print("Il a " + str(l))
+#can convert to float with float() function
+
